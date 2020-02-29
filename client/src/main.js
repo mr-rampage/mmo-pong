8fa8e85a11d3f97d25501df23c5c33cab1b95f72
mr-rampage <readonly@che.wbac.ca>
2020-02-29 12:48:41 -0500

    adding playing field

--- client/src/main.js
+++ client/src/main.js
@@ -8,3 +8,21 @@ import { controllerSource } from './controller';
 
 const direction = controllerSource();
 observe(msg => console.log(msg))(direction);
+
+const gameState = {
+    player1: {
+        x: 0,
+        y: 0.5,
+        l: 0.1
+    },
+    player2: {
+        x: 1,
+        y: 0.5,
+        l: 0.1
+    },
+    ball: {
+        x: 0.5,
+        y: 0.5,
+        r: 0.01
+    }
+};
