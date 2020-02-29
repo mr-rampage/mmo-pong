c612ce798dda753b01516d8d51bc11830d0cfb0e
Camilo Bernal <camilo.bernal@intelliware.ca>
2020-02-29 17:24:22 -0500

    assign to other team

--- node-server.js
+++ node-server.js
@@ -78,7 +78,7 @@ export default class NodeServer {
     const playerId = uuidv4();
     socket.playerId = playerId;
     socket.direction = 0;
-    this.leftPlayers[playerId] = socket;
+    team[playerId] = socket;
   }
 
   getAllPlayerStates() {
