2df58b8cfd98a0a10b7c2ffdd9aafb138241df0c
Camilo Bernal <camilo.bernal@intelliware.ca>
2020-02-29 15:46:59 -0500

    store game state

--- simulator.js
+++ simulator.js
@@ -1,129 +1,152 @@
-// world
-var worldWidth = 1;
-var worldHeight = 1;
-var padding = 0.01;
+export default class Game {
+  constructor() {
+    // world
+    this.worldWidth = 1;
+    this.worldHeight = 1;
+    this.padding = 0.01;
 
-// ball
-var x = worldWidth / 2;
-var y = 0;
-var dx = 0.04;
-var dy = 0.04;
-var ballRadius = 0.1;
+    // ball
+    this.x = worldWidth / 2;
+    this.y = 0;
+    this.dx = 0.04;
+    this.dy = 0.04;
+    this.ballRadius = 0.1;
 
-// left player paddle
-var leftPaddleHeight = 0.3;
-var leftPaddleWidth = 0.1;
-var leftPaddleX = padding;
-var leftPaddleY = worldHeight / 2 - leftPaddleHeight / 2;
+    // left player paddle
+    this.leftPaddleHeight = 0.3;
+    this.leftPaddleWidth = 0.1;
+    this.leftPaddleX = padding;
+    this.leftPaddleY = worldHeight / 2 - leftPaddleHeight / 2;
 
-// Right player paddle
-var rightPaddleHeight = 0.3;
-var rightPaddleWidth = 0.1;
-var rightPaddleX = worldWidth - (rightPaddleWidth + padding);
-var rightPaddleY = worldHeight / 2 - rightPaddleHeight / 2;
+    // Right player paddle
+    this.rightPaddleHeight = 0.3;
+    this.rightPaddleWidth = 0.1;
+    this.rightPaddleX = worldWidth - (rightPaddleWidth + padding);
+    this.rightPaddleY = worldHeight / 2 - rightPaddleHeight / 2;
 
-// boolean to handle direction changes
-var leftUpPressed = false;
-var leftDownPressed = false;
-var rightUpPressed = false;
-var rightDownPressed = false;
+    // boolean to handle direction changes
+    this.leftUpPressed = false;
+    this.leftDownPressed = false;
+    this.rightUpPressed = false;
+    this.rightDownPressed = false;
 
-var leftScore = 0;
-var rightScore = 0;
+    // game score
+    this.leftScore = 0;
+    this.rightScore = 0;
+  }
 
-
-function handleInput({ leftPaddle, rightPaddle }){
-    switch(leftPaddle.direction) {
-        case 'UP':
-            leftUpPressed = true;
-            break;
-        case 'DOWN':
-            leftDownPressed = true;
-            break;
-        case 'STOP':
-            leftUpPressed = false;
-            leftDownPressed = false;
-            break;
-        default:
-            break;
+  handleInput({ leftPaddle, rightPaddle }) {
+    switch (leftPaddle.direction) {
+      case "UP":
+        this.leftUpPressed = true;
+        break;
+      case "DOWN":
+        this.leftDownPressed = true;
+        break;
+      case "STOP":
+        this.leftUpPressed = false;
+        this.leftDownPressed = false;
+        break;
+      default:
+        break;
     }
 
-    switch(rightPaddle.direction) {
-        case 'UP':
-            rightUpPressed = true;
-            break;
-        case 'DOWN':
-            rightDownPressed = true;
-            break;
-        case 'STOP':
-            rightUpPressed = false;
-            rightDownPressed = false;
-            break;
-        default:
-            break;
-    }
-    
-}
-
-
-function collisionsWithLeftPaddle() {
-  if (x - ballRadius <= padding + leftPaddleWidth) {
-    if (y > leftPaddleY && y < leftPaddleY + leftPaddleHeight) dx = -dx;
-    else if (x - ballRadius <= 0) {
-      rightScore++;
-      x = worldWidth / 2;
-      y = worldHeight / 2;
-      dx = -dx;
-      dy = -dy;
+    switch (rightPaddle.direction) {
+      case "UP":
+        this.rightUpPressed = true;
+        break;
+      case "DOWN":
+        this.rightDownPressed = true;
+        break;
+      case "STOP":
+        this.rightUpPressed = false;
+        this.rightDownPressed = false;
+        break;
+      default:
+        break;
     }
   }
-}
 
-function collisionsWithRightPaddle() {
-  if (x + ballRadius >= worldWidth - (rightPaddleWidth + padding)) {
-    if (y > rightPaddleY && y < rightPaddleY + rightPaddleHeight) dx = -dx;
-    else if (x + ballRadius >= worldWidth) {
-      leftScore++;
-      //alert("Game Over");
-      x = worldWidth / 2;
-      y = worldHeight / 2;
-      dx = -dx;
-      dy = -dy;
-      //document.location.reload();
+  collisionsWithLeftPaddle() {
+    if (this.x - this.ballRadius <= this.padding + this.leftPaddleWidth) {
+      if (
+        this.y > this.leftPaddleY &&
+        this.y < this.leftPaddleY + this.leftPaddleHeight
+      )
+        this.dx = -this.dx;
+      else if (this.x - this.ballRadius <= 0) {
+        this.rightScore++;
+        this.x = this.worldWidth / 2;
+        this.y = worldHeight / 2;
+        this.dx = -this.dx;
+        this.dy = -this.dy;
+      }
     }
   }
-}
 
-function computeCollisionsWithWallsAndPaddle() {
-  collisionsWithLeftPaddle();
-  collisionsWithRightPaddle();
-  if (y - ballRadius <= 0 || y + ballRadius >= worldHeight) {
-    dy = -dy;
+  collisionsWithRightPaddle() {
+    if (
+      this.x + this.ballRadius >=
+      this.worldWidth - (this.rightPaddleWidth + this.padding)
+    ) {
+      if ( this.y > this.rightPaddleY && this.y < this.rightPaddleY + this.rightPaddleHeight)
+        this.dx = -this.dx;
+      else if (this.x + this.ballRadius >= this.worldWidth) {
+        this.leftScore++;
+        //alert("Game Over");
+        this.x = worldWidth / 2;
+        this.y = worldHeight / 2;
+        this.dx = -dx;
+        this.dy = -dy;
+        //document.location.reload();
+      }
+    }
   }
-}
 
-function updateLeftPaddle() {
-  if (leftDownPressed && leftPaddleY < worldHeight - leftPaddleHeight) {
-    leftPaddleY += 0.7;
-  } else if (leftUpPressed && leftPaddleY > 0) {
-    leftPaddleY -= 0.7;
+  computeCollisionsWithWallsAndPaddle() {
+    collisionsWithLeftPaddle();
+    collisionsWithRightPaddle();
+    if (
+      this.y - this.ballRadius <= 0 ||
+      this.y + this.ballRadius >= this.worldHeight
+    ) {
+      this.dy = -dy;
+    }
   }
-}
 
-function updateRightPaddle() {
-  if (rightDownPressed && rightPaddleY < worldHeight - rightPaddleHeight) {
-    rightPaddleY += 0.7;
-  } else if (rightUpPressed && rightPaddleY > 0) {
-    rightPaddleY -= 0.7;
+  updateLeftPaddle() {
+    if (
+      this.leftDownPressed &&
+      this.leftPaddleY < this.worldHeight - this.leftPaddleHeight
+    ) {
+      this.leftPaddleY += 0.7;
+    } else if (this.leftUpPressed && this.leftPaddleY > 0) {
+      this.leftPaddleY -= 0.7;
+    }
   }
-}
 
-export function simulate({ leftPaddleDirection, rightPaddleDirection }) {
-  handleInput({ leftPaddleDirection, rightPaddleDirection });
-  updateLeftPaddle();
-  updateRightPaddle();
-  computeCollisionsWithWallsAndPaddle();
-  x += dx;
-  y += dy;
-  return { ballX: x, ballY: y, leftScore, rightScore };
+  updateRightPaddle() {
+    if (
+      this.rightDownPressed &&
+      this.rightPaddleY < this.worldHeight - this.rightPaddleHeight
+    ) {
+      this.rightPaddleY += 0.7;
+    } else if (this.rightUpPressed && this.rightPaddleY > 0) {
+      this.rightPaddleY -= 0.7;
+    }
+  }
+  simulate({ leftPaddleDirection, rightPaddleDirection }) {
+    handleInput({ leftPaddleDirection, rightPaddleDirection });
+    updateLeftPaddle();
+    updateRightPaddle();
+    computeCollisionsWithWallsAndPaddle();
+    this.x += dx;
+    this.y += dy;
+    return {
+      ballX: this.x,
+      ballY: this.y,
+      leftScore: this.leftScore,
+      rightScore: this.rightScore
+    };
+  }
 }
