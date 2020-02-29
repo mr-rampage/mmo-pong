73e79a8c0ea974fb6934fe1ad841d5fcd80b6329
mr-rampage <readonly@che.wbac.ca>
2020-02-29 11:20:47 -0500

    adding simple ws client

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,6 @@
-import update from './update.js';
+import websocket from 'callbag-websocket';
+import observe from 'callbag-observe';
 
-// even though Rollup is bundling all your files together, errors and
-// logs will still point to your original source modules
-console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
+const ws = websocket('ws://' + location.hostname + '/');
 
-update();
+observe(msg => console.log('obs1', msg.data))(ws);
