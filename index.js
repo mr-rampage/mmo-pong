cf21c9509b0e0fa69f33ec682d883320c440b018
Kevin Chan <kvii.chan@gmail.com>
2020-02-29 10:59:30 -0500

    added babel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var http = require('http');
+import http from 'http';
 
-http.createServer(function (request, response) {
+http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
 }).listen(8080);
